1bbf6e260c0eb553499ead2691651bba90b7fcf2
DevSuzzy <egeonususan6@gmail.com>
2023-09-14 11:03:34 +0100

    initial commit

---
+++ hr-system-backend/src/main/java/com/hrsupportcentresq014/dtos/response/EmployeeViewProfileResponse.java
@@ -0,0 +1,39 @@
+package com.hrsupportcentresq014.dtos.response;
+
+
+import com.hrsupportcentresq014.entities.NextOfKin;
+import com.hrsupportcentresq014.entities.Role;
+import com.hrsupportcentresq014.utils.Social;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeViewProfileResponse {
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String position;
+    private String phoneNo;
+    private String email;
+    private LocalDate startDate;
+    private Social social;
+    private String reportTo;
+    private String resumeUrl;
+    private LocalDate birthday;
+    private String address;
+    private NextOfKin nextOfKin;
+    private String maritalStatus;
+    private String contractType;
+    private String imageUrl;
+    private String workLocation;
+    private Role role;
+    private String department;
+    private String nationality;
+    private LocalDateTime createdOn;
+}
